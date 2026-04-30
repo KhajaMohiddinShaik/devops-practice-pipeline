1d7d3c52f998e8242269f034e4f62b462bc05b7e
Arya <shaikkhazamohiddin@gmail.com>
2026-04-30 16:26:32 +0000

    checking if testing code actually works

--- test_main.py
+++ test_main.py
@@ -7,3 +7,4 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello from CI/CD"}
+#python test file written with pytest
