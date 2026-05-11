3ea6708d8620fab248d41bb59e7f1c8ef642a030
Arya <shaikkhazamohiddin@gmail.com>
2026-05-11 13:33:50 +0000

    Fixed code format according to Lint feedback-2

--- test_main.py
+++ test_main.py
@@ -3,10 +3,11 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello from CI/CD"}
-#python test file written with pytest
-#Automated CI till Docker Registry
-#Jenkins is now in WSL
+# python test file written with pytest
+# Automated CI till Docker Registry
+# Jenkins is now in WSL
